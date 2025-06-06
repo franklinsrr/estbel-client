488d554c26f3c64900179f8946282d67bf44324b
Franklin Rodriguez <franklinserif@gmail.com>
2025-06-06 17:12:23 -0400

    feat(dashboard): replace static CardStats with dynamic rendering from mock data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from '@components/navbat';
 import CardStats from '@components/CardStats';
+import { cardStatsMock } from '@/constants/mock/card';
 
 export default function Dashboard() {
   return (
@@ -8,9 +9,9 @@ export default function Dashboard() {
 
       <div className="flex flex-col gap-4 mt-10 px-10">
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          <CardStats />
-          <CardStats />
-          <CardStats />
+          {cardStatsMock.map(card => (
+            <CardStats key={card.id} {...card} />
+          ))}
         </section>
       </div>
     </div>
