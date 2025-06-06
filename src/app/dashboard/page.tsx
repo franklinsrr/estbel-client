7eed937cd7a5a89ed4ab0424ce8b2428d443f7ea
Franklin Rodriguez <franklinserif@gmail.com>
2025-06-06 16:58:44 -0400

    feat(dashboard): integrate CardStats component into Dashboard layout for enhanced statistics display

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,18 @@
 import Navbar from '@components/navbat';
+import CardStats from '@components/CardStats';
 
 export default function Dashboard() {
   return (
     <div>
       <Navbar />
 
-      <div className="flex flex-col gap-4">dashboard</div>
+      <div className="flex flex-col gap-4 mt-10 px-10">
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <CardStats />
+          <CardStats />
+          <CardStats />
+        </section>
+      </div>
     </div>
   );
 }
