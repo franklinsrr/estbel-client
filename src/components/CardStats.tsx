915cb651194b9993526e5138a0d5cba2d2604289
Franklin Rodriguez <franklinserif@gmail.com>
2025-06-06 17:12:17 -0400

    feat(CardStats): enhance CardStats component with props for dynamic content and trend indicators

--- src/components/CardStats.tsx
+++ src/components/CardStats.tsx
@@ -7,30 +7,50 @@ import {
   CardFooter,
 } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
-import { TrendingUp } from 'lucide-react';
+import { TrendingUp, TrendingDown } from 'lucide-react';
 
-const CardStats = () => {
+export interface CardStatsProps {
+  title: string;
+  value: string;
+  percentage: string;
+  description: string;
+  longDescription: string;
+}
+
+const CardStats: React.FC<CardStatsProps> = ({
+  title,
+  value,
+  percentage,
+  description,
+  longDescription,
+}) => {
   return (
     <Card className="@container/card">
       <CardHeader>
-        <CardDescription>Total Revenue</CardDescription>
+        <CardDescription>{title}</CardDescription>
         <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-          $1,250.00
+          {value}
         </CardTitle>
         <CardAction>
-          <Badge variant="outline">
-            <TrendingUp />
-            +12.5%
+          <Badge
+            variant={
+              percentage.includes('+') || percentage.includes('-')
+                ? 'default'
+                : 'destructive'
+            }
+          >
+            {percentage.includes('+') ? <TrendingUp /> : <TrendingDown />}
+            {percentage}
           </Badge>
         </CardAction>
       </CardHeader>
       <CardFooter className="flex-col items-start gap-1.5 text-sm">
         <div className="line-clamp-1 flex gap-2 font-medium">
-          Trending up this month <TrendingUp className="size-4" />
+          {description}
+          {percentage.includes('+') && <TrendingUp className="size-4" />}
+          {percentage.includes('-') && <TrendingDown className="size-4" />}
         </div>
-        <div className="text-muted-foreground">
-          Visitors for the last 6 months
-        </div>
+        <div className="text-muted-foreground">{longDescription}</div>
       </CardFooter>
     </Card>
   );
