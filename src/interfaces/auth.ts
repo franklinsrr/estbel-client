5e2140bc5b254e087aaa75c660e3dbcca1b1ed82
Franklin Rodriguez <franklinserif@gmail.com>
2025-06-21 22:39:21 -0400

    feat(auth): add IAuth and IAuthResponse interfaces, and update type exports in auth.ts

--- src/interfaces/auth.ts
+++ src/interfaces/auth.ts
@@ -1,10 +1,20 @@
-import * as z from "zod";
-import { forgotPasswordSchema, formSignInSchema } from "@/schemas/auth";
+import * as z from 'zod';
+import { forgotPasswordSchema, formSignInSchema } from '@/schemas/auth';
 
-export type TSignInSchema = z.infer<typeof formSignInSchema>
-export type TForgotPassworSchema = z.infer<typeof forgotPasswordSchema>
+export type TSignInSchema = z.infer<typeof formSignInSchema>;
+export type TForgotPassworSchema = z.infer<typeof forgotPasswordSchema>;
 
 export interface TChangePasswordSchema {
   provisionalPassword: string;
   newPassword: string;
+}
+
+export interface IAuthResponse {
+  accessToken: string;
+}
+
+export interface IAuth {
+  sub: string;
+  iat: number;
+  exp: number;
 }
