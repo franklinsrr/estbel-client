ce9e4316c492ad32b5ae07b2a0b73a3416e443a6
Franklin Rodriguez <franklinserif@gmail.com>
2025-06-22 21:49:17 -0400

    feat(auth): add default values for authentication forms and initial auth state

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,13 +1,37 @@
+import { IAuthStore } from '@/interfaces/auth';
+
+/**
+ * SIGNIN_DEFAULT_VALUES is the default values for the sign in form.
+ * @type {TSignInSchema}
+ */
 export const SIGNIN_DEFAULT_VALUES = {
-    email: "",
-    password: "",
-}
+  email: '',
+  password: '',
+};
 
+/**
+ * FORGOT_PASSWORD_DEFAULT_VALUES is the default values for the forgot password form.
+ * @type {TForgotPassworSchema}
+ */
 export const FORGOT_PASSWORD_DEFAULT_VALUES = {
-    email: ""
-}
+  email: '',
+};
 
+/**
+ * CHANGE_PASSWORD_DEFAULT_VALUES is the default values for the change password form.
+ * @type {TChangePasswordSchema}
+ */
 export const CHANGE_PASSWORD_DEFAULT_VALUES = {
-    provisionalPassword: "",
-    newPassword: ""
-}
+  provisionalPassword: '',
+  newPassword: '',
+};
+
+/**
+ * INITIAL_AUTH_STATE is the initial state for the authentication store.
+ * @type {IAuthStore}
+ */
+export const INITIAL_AUTH_STATE: IAuthStore = {
+  auth: null,
+  setAuth: () => {},
+  clearAuth: () => {},
+};
