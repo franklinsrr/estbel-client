4c1f5e29c51f4362c5e204433cec4ed1b3b29acb
Franklin Rodriguez <franklinserif@gmail.com>
2025-06-30 01:07:43 -0400

    feat(birthday): add BirthdayCard and BirthdaySkeleton components for displaying birthday information and loading states

---
+++ src/store/stats/useStatsBirthdaySlice.ts
@@ -0,0 +1,37 @@
+import { StateCreator } from 'zustand';
+import { IBirthdaySlice } from '@/interfaces/birthday';
+import { TStore } from '@/interfaces/store';
+import { INITIAL_BIRTHDAY_STATE } from '@/constants/birthday';
+import { httpBirthdayClient } from '@/http/dashboardHome/httpBirthdayClient';
+
+/**
+ * useStatsBirthdaySlice is a Zustand slice that manages the birthday state.
+ */
+export const useStatsBirthdaySlice: StateCreator<
+  TStore,
+  [['zustand/subscribeWithSelector', never], ['zustand/immer', never]],
+  [],
+  IBirthdaySlice
+> = set => ({
+  ...INITIAL_BIRTHDAY_STATE,
+
+  /**
+   * Get the birthday people
+   */
+  getBirthdays: async () => {
+    set({ isLoading: true });
+    try {
+      const res = await httpBirthdayClient.getBirthday();
+      set({ birthday: res, error: null, isLoading: false });
+    } catch (error) {
+      set({ error: error as string, isLoading: false });
+    }
+  },
+
+  /**
+   * Clear the birthday state.
+   */
+  clearBirthdays: () => {
+    set({ birthday: [], error: null });
+  },
+});
