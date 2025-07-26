a1a6e5296a2446128ae686dcdd32517d82e054b9
Franklin Rodriguez <franklinserif@gmail.com>
2025-07-26 04:41:10 -0400

    feat(birthday): refactor birthday HTTP handling into HttpBirthdayClient class for improved structure and maintainability

--- src/http/dashboardHome/httpBirthdayClient.ts
+++ src/http/dashboardHome/httpBirthdayClient.ts
@@ -1,16 +1,24 @@
 import { httpClient } from '@/http/httpClient';
 import { IMember } from '@/interfaces/member';
 import { CURRENT_MONTH, CURRENT_DAY } from '@/constants/birthday';
+import { AxiosInstance } from 'axios';
 
-export const httpBirthdayClient = {
+/**
+ * HttpBirthdayClient is a class that handles the HTTP requests for the birthday.
+ */
+export class HttpBirthdayClient {
+  constructor(private readonly httpClient: AxiosInstance) {}
+
   /**
    * Get the birthday people
    * @returns {Promise<IMember[]>} The birthday people
    */
-  getBirthday: async (): Promise<IMember[]> => {
-    const res = await httpClient.get<IMember[]>(
+  getBirthday = async (): Promise<IMember[]> => {
+    const res = await this.httpClient.get<IMember[]>(
       `/stats/birthday/${CURRENT_MONTH}/${CURRENT_DAY}`
     );
     return res.data;
-  },
-};
+  };
+}
+
+export const httpBirthdayClient = new HttpBirthdayClient(httpClient);
