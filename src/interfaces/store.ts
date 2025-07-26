b778fad93abad704af193686b0dde0adaaab7d8f
Franklin Rodriguez <franklinserif@gmail.com>
2025-07-26 04:40:18 -0400

    refactor(store): update TStore type to include multiple slices and improve error handling

--- src/interfaces/store.ts
+++ src/interfaces/store.ts
@@ -1,3 +1,17 @@
-import { IAuthStore } from './auth';
+import { IAuthSlice } from './auth';
+import { AttendanceChartHistoryState } from './chart';
+import { IBirthdaySlice } from './birthday';
+import { IEventsTableState } from './events';
 
-export type TStore = IAuthStore;
+export type TStore = Omit<IAuthSlice, 'error'> &
+  Omit<AttendanceChartHistoryState, 'historyError'> &
+  Omit<IBirthdaySlice, 'error'> &
+  Omit<IEventsTableState, 'eventsError'> &
+  AttendanceChartHistoryState &
+  IEventsTableState & {
+    error:
+      | IAuthSlice['error']
+      | AttendanceChartHistoryState['historyError']
+      | IBirthdaySlice['error']
+      | IEventsTableState['eventsError'];
+  };
