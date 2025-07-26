5adb5a2314632fb59a5828c57199f6c9df41f179
Franklin Rodriguez <franklinserif@gmail.com>
2025-07-26 12:23:31 -0400

    refactor(dashboard): adjust CardStats layout to conditionally span columns based on index for improved responsiveness

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -17,8 +17,13 @@ export default function Dashboard() {
 
       <div className="flex flex-col gap-4 mt-10 px-10">
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {cardStatsMock.map(card => (
-            <CardStats key={card.id} {...card} />
+          {cardStatsMock.map((card, index) => (
+            <div
+              key={card.id}
+              className={`${index === cardStatsMock.length - 1 ? 'md:col-span-2 lg:col-span-1' : ''}`}
+            >
+              <CardStats {...card} />
+            </div>
           ))}
         </section>
         <div className="flex items-center justify-center relative">
